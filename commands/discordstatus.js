983497d8240feace95b2bb8dd6a8c76425e5cbbf
buldermangg-alt <71522932+buldermangg-alt@users.noreply.github.com>
2020-09-22 18:27:16 +0300

    Update discordstatus.js

--- commands/discordstatus.js
+++ commands/discordstatus.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'discordstatus',
     description: "See discord status!",
     execute(message,args){
-message.channel.send('https://discordstatus.com/#day');
+message.channel.send({ content: "", embed: {title: "https://discordstatus.com/#day"}})
     }
 }
