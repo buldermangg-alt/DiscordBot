94778e4eb334e6c21d3e0025577f504e88a9f440
buldermangg-alt <71522932+buldermangg-alt@users.noreply.github.com>
2020-09-23 11:02:47 +0300

    Delete open_source.js

--- open_source.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'open_source',
-    description: "The bot code!",
-    execute(message,args){
-message.channel.send({ content: "", embed: {title: "https://github.com/buldermangg-alt/DiscordBot"}})
-    }
-}
