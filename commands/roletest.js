0f2aa265a758ac1e096d9c85bd55c1f81466f978
Buldermangg <71522932+buldermangg-alt@users.noreply.github.com>
2020-09-26 10:58:48 +0300

    Update roletest.js

--- commands/roletest.js
+++ commands/roletest.js
@@ -1,14 +1,7 @@
 module.exports = {
     name: 'roletest',
-    description: "Role test commands!",
+    description: "This is a role test command!",
     execute(message,args){
-
-        if (message.member.roles.cache.has('756075508063666297')){
-            message.channel.send('ur mod!')
-        }else {
-            message.channel.send('You dont have the right permissions to use this command let me change that!').catch(console.error);
-        }
-
-
+message.channel.send({ content: "", embed: {title: "This command is under development."}})
     }
 }
