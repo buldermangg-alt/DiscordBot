a3354bdccde5451b286d08d733d36feb2bfa56ea
Buldermangg <71522932+buldermangg-alt@users.noreply.github.com>
2020-10-01 09:25:39 +0300

    Delete roletest.js

--- commands/roletest.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'roletest',
-    description: "This is a role test command!",
-    execute(message,args){
-message.channel.send({ content: "", embed: {title: "This command is under development."}})
-    }
-}
