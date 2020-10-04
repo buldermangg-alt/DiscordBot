87a0ee0f758bee1678605acaaf4f678463bc19bc
Buldermangg <71522932+buldermangg-alt@users.noreply.github.com>
2020-10-04 21:39:02 +0300

    Delete youtube.js

--- commands/youtube.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'youtube',
-    description: "My youtube channel!",
-    execute(message,args){
-message.channel.send('This command is currently distabled :/');
-    }
-}
