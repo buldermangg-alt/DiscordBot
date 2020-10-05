4689fbd4f1cc52feb1cccab33ade2bae02b3b515
Buldermangg <71522932+buldermangg-alt@users.noreply.github.com>
2020-10-05 16:02:55 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+const os = require('os');
+const fs = require('fs');
+const { Client, MessageEmbed } = require('discord.js');
 const Discord = require('discord.js');
 const client = new Discord.Client();
  
 const prefix = '-';
  
-const fs = require('fs');
  
 client.commands = new Discord.Collection();
  
@@ -48,6 +50,10 @@ client.on('message', message =>{
         client.commands.get('reload').execute(message, args);
     } else if (command == 'prune'){
         client.commands.get('prune').execute(message, args);
+    } else if (command == 'uptime'){
+        client.commands.get('uptime').execute(message,args,client);
+    } else if (command == 'status'){
+        client.commands.get('status').execute(message,args,client,MessageEmbed,fs,os);
     }
 
 });
