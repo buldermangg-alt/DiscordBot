0ba458ab80717e489e7e1d33ffa6f29d8b90fafd
Buldermangg <71522932+buldermangg-alt@users.noreply.github.com>
2020-10-09 22:43:41 +0300

    Delete todolist.js

--- commands/todolist.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'todolist',
-    description: "The upcoming commands and things!",
-    execute(message,args){
-message.channel.send({ content: "", embed: {title: "New commands coming soon!"}})
-    }
-}
