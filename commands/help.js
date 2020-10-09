fd48648c1e40b115865504a7f50c43986a7f8abf
Buldermangg <71522932+buldermangg-alt@users.noreply.github.com>
2020-10-09 22:44:36 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'help',
     description: "List of the commands!",
     execute(message,args){
-message.channel.send({ content: "The commands", embed: {title: "Current commands: \n -test  \n -roletest  \n -todolist  \n -verify \n -discordstatus \n -help  \n -credits"}})
+message.channel.send({ content: "The commands", embed: {title: "-avatar \n-server \n-prune \n-help \n-test \n-todolist \n-uptime \n-open_source \n-reload \n-discordstatus \n-credits \n"}})
     }
 }
